8f64d0c0c6415737d0e43d1d257b15c536d48ca0
Oleksandr Terentiev <st327416@gmail.com>
2025-07-31 20:25:49 +0200

    added file

---
+++ password_generator.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+def gen_password(lenght , numbers , letters , symbols):
+    char = ''
+
+    if numbers:
+        char += string.digits
+    if letters:
+        char += string.ascii_letters
+    if symbols:
+        char += string.punctuation
+
+    if not char:
+        return "Select at least 1 type of symbols"
+
+    password=("".join(random.choice(char)for _ in range (lenght)))
+
+    return password
+
+
+try:
+    length=int(input("Write length of your password: "))
+    if length<=0 or not length.is_integer():
+        print("Error")
+    else:
+        letters = input("Include letters?yes/no: ").lower() == "yes"
+        numbers = input("Include numbers?yes/no: ").lower() == "yes"
+        symbols = input("Include symblos?yes/no: ").lower() == "yes"
+
+        password=gen_password(length, numbers ,  letters , symbols)
+        print(f"Your password: {password}")
+except ValueError:
+    print("Error!")
+
